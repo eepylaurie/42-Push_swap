af60a938ee9d9350741f2e13746aec00460df09b
eepylaurie <eepylaurie@gmail.com>
2026-01-16 22:17:06 +0100

    Add header file for push_swap with node structure definition

---
+++ inc/push_swap.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/16 21:43:32 by lmatthes          #+#    #+#             */
+/*   Updated: 2026/01/16 22:16:42 by lmatthes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+typedef struct s_node
+{
+	int				value;
+	int				index;
+	struct s_node	*next;
+	struct s_node	*prev;
+}	t_node;
+
+// function prototypes
+
+#endif
