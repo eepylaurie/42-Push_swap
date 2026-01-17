36d3465f4152dc6861b340c68de7f99ccf2435c7
eepylaurie <eepylaurie@gmail.com>
2026-01-17 17:39:18 +0100

    Refactor main function to include print_stack for improved stack visualization

--- src/main.c
+++ src/main.c
@@ -6,24 +6,36 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 15:20:22 by lmatthes          #+#    #+#             */
-/*   Updated: 2026/01/17 16:54:53 by lmatthes         ###   ########.fr       */
+/*   Updated: 2026/01/17 17:32:53 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
+static void	print_stack(t_stack *s)
+{
+	t_node *cur = s->top;
+
+	printf("size= %d top=%d bot=%d | ", s->size,
+		s->top ? s->top->val : -999,
+		s->bot ? s->bot->val : -999);
+	while (cur)
+	{
+		printf("%d ", cur->val);
+		cur = cur->next;
+	}
+	printf("\n");
+}
+
 int	main(void)
 {
 	t_stack	a;
-	t_node	*n;
 
 	stack_init(&a);
-	n = node_new(42);
-	if (!n)
-		return (1);
-	printf("node=%p value=%d index=%d next=%p prev=%p\n",
-		(void *)n, n->value, n->index, (void *)n->next, (void *)n->prev);
-	free(n);
+	stack_push_top(&a, node_new(3));
+	stack_push_top(&a, node_new(2));
+	stack_push_top(&a, node_new(1));
+	print_stack(&a);
 	return (0);
 }
