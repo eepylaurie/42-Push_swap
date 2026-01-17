369bcab112a5bf48603e262e91b5157f752b864d
eepylaurie <eepylaurie@gmail.com>
2026-01-17 18:04:58 +0100

    Update header file to include stack_add_bottom function prototype

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 21:43:32 by lmatthes          #+#    #+#             */
-/*   Updated: 2026/01/17 17:44:11 by lmatthes         ###   ########.fr       */
+/*   Updated: 2026/01/17 18:04:42 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,6 @@ void	stack_init(t_stack *s);
 t_node	*node_new(int val);
 void	stack_push_top(t_stack *s, t_node *n);
 t_node	*stack_pop_top(t_stack *s);
+void	stack_add_bottom(t_stack *s, t_node *n);
 
 #endif
