3d4dd0c5e51c81716cb4573050494c4f789b471b
eepylaurie <eepylaurie@gmail.com>
2026-01-17 16:55:18 +0100

    Refactor main function to initialize stack and create a new node for testing

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 15:20:22 by lmatthes          #+#    #+#             */
-/*   Updated: 2026/01/17 15:24:29 by lmatthes         ###   ########.fr       */
+/*   Updated: 2026/01/17 16:54:53 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,14 @@
 int	main(void)
 {
 	t_stack	a;
+	t_node	*n;
 
-	a.head = (void *)1;
-	a.tail = (void *)1;
-	a.size = 123;
 	stack_init(&a);
-	printf("head=%p tail=%p size=%d\n", (void *)a.head, (void *)a.tail, a.size);
+	n = node_new(42);
+	if (!n)
+		return (1);
+	printf("node=%p value=%d index=%d next=%p prev=%p\n",
+		(void *)n, n->value, n->index, (void *)n->next, (void *)n->prev);
+	free(n);
 	return (0);
 }
