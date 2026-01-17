d9340c1f49526d8a6ae5e01b1bb3a8ea4523509f
eepylaurie <eepylaurie@gmail.com>
2026-01-17 20:55:36 +0100

    Add reverse rotate operations

--- src/ops/instructions_rev_rotate.c
+++ src/ops/instructions_rev_rotate.c
@@ -6,7 +6,7 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 20:36:11 by lmatthes          #+#    #+#             */
-/*   Updated: 2026/01/17 20:49:18 by lmatthes         ###   ########.fr       */
+/*   Updated: 2026/01/17 20:51:56 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,23 @@ static void	op_rev_rotate(t_stack *s)
 	last->next = s->top;
 	s->top->prev = last;
 	s->top = last;
+}
+
+void	rra(t_stack *a)
+{
+	op_rev_rotate(a);
+	write(1, "rra\n", 4);
+}
+
+void	rrb(t_stack *b)
+{
+	op_rev_rotate(b);
+	write(1, "rrb\n", 4);
+}
+
+void	rrr(t_stack *a, t_stack *b)
+{
+	op_rev_rotate(a);
+	op_rev_rotate(b);
+	write(1, "rrr\n", 4);
 }
