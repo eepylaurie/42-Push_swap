1b552658eaffc4c2befe51d1fe80d86c2d698244
eepylaurie <eepylaurie@gmail.com>
2026-01-17 18:21:17 +0100

    Update header file with stack_is_sorted function prototype

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 21:43:32 by lmatthes          #+#    #+#             */
-/*   Updated: 2026/01/17 18:04:42 by lmatthes         ###   ########.fr       */
+/*   Updated: 2026/01/17 18:19:55 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,6 @@ t_node	*node_new(int val);
 void	stack_push_top(t_stack *s, t_node *n);
 t_node	*stack_pop_top(t_stack *s);
 void	stack_add_bottom(t_stack *s, t_node *n);
+int		stack_is_sorted(t_stack *s);
 
 #endif
