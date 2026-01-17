4bad072295bf92468d383ebdff506df7069203e8
eepylaurie <eepylaurie@gmail.com>
2026-01-17 20:35:12 +0100

    Implement rotate operations

--- src/ops/instructions_rotate.c
+++ src/ops/instructions_rotate.c
@@ -6,7 +6,7 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 20:14:50 by lmatthes          #+#    #+#             */
-/*   Updated: 2026/01/17 20:18:48 by lmatthes         ###   ########.fr       */
+/*   Updated: 2026/01/17 20:29:03 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,23 @@ static void	op_rotate(t_stack *s)
 		return ;
 	n = stack_pop_top(s);
 	stack_add_bottom(s, n);
+}
+
+void	ra(t_stack *a)
+{
+	op_rotate(a);
+	write(1, "ra\n", 3);
+}
+
+void	rb(t_stack *b)
+{
+	op_rotate(b);
+	write(1, "rb\n", 3);
+}
+
+void	rr(t_stack *a, t_stack *b)
+{
+	op_rotate(a);
+	op_rotate(b);
+	write(1, "rr\n", 3);
 }
