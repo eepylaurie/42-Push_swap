6ef23d023d29e7e3584da027d0de2ff59fa39745
eepylaurie <eepylaurie@gmail.com>
2026-01-17 23:06:33 +0100

    Remove test function and related calls from main.c, testing only error_exit

--- src/main.c
+++ src/main.c
@@ -6,42 +6,15 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 15:20:22 by lmatthes          #+#    #+#             */
-/*   Updated: 2026/01/17 21:52:15 by lmatthes         ###   ########.fr       */
+/*   Updated: 2026/01/17 23:05:03 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-static void	test(const char *s)
-{
-	int	ok;
-	int	v;
-
-	v = atoi_strict(s, &ok);
-	printf("input: %-12s | ok=%d | val=%d\n",
-		s ? s : "NULL", ok, v);
-}
-
 int	main(void)
 {
-	test("0");
-	test("42");
-	test("+42");
-	test("-42");
-	test("0042");
-	test("2147483647");
-	test("-2147483648");
-	test("");
-	test("+");
-	test("-");
-	test("++1");
-	test("--1");
-	test("42a");
-	test("a42");
-	test("2147483648");
-	test("-2147483649");
-	test("1 2");
-	test(" ");
+	error_exit();
 	return (0);
 }
