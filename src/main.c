a8274e042f145bf9ec50f81571c95a2bdb578b33
eepylaurie <eepylaurie@gmail.com>
2026-01-19 16:18:03 +0100

    Refactor main function to test argument parsing

--- src/main.c
+++ src/main.c
@@ -6,38 +6,22 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 15:20:22 by lmatthes          #+#    #+#             */
-/*   Updated: 2026/01/18 20:17:34 by lmatthes         ###   ########.fr       */
+/*   Updated: 2026/01/19 16:08:33 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-static void	test_one(const char *p, size_t len)
+int	main(int argc, char **argv)
 {
-	int	ok;
-	int	v;
+	t_stack	a;
+	t_stack	b;
 
-	v = atoi_strict_span(p, len, &ok);
-	printf("span='%.*s' len=%zu -> ok=%d, v=%d\n",
-		(int)len, p, len, ok, v);
-}
-
-int	main(void)
-{
-	test_one("6", 1);
-	test_one("-2", 2);
-	test_one("+0", 2);
-	test_one("0", 1);
-	test_one("+", 1);
-	test_one(" 3", 2);
-	test_one("3 ", 2);
-	test_one("6 3", 3);
-	test_one("2147483647", 10);
-	test_one("2147483648", 10);
-	test_one("-2147483648", 11);
-	test_one("-2147483649", 11);
-	test_one("--3", 3);
-	test_one("3-2", 3);
+	stack_init(&a);
+	stack_init(&b);
+	parse_args(argc, argv, &a);
+	stack_clear(&a);
+	stack_clear(&b);
 	return (0);
 }
