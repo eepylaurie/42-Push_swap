5df8562056fecb0c7462ae298ffe61b554999407
eepylaurie <eepylaurie@gmail.com>
2026-01-19 16:55:36 +0100

    Refactor main function to print stack values after indexing

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 15:20:22 by lmatthes          #+#    #+#             */
-/*   Updated: 2026/01/19 16:24:25 by lmatthes         ###   ########.fr       */
+/*   Updated: 2026/01/19 16:47:57 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,21 @@ int	main(int argc, char **argv)
 	stack_init(&a);
 	stack_init(&b);
 	parse_args(argc, argv, &a);
-	if (is_sorted(&a))
+	index_stack(&a);
+	t_node	*cur = a.top;
+	while (cur)
 	{
-		write(1, "SORTED\n", 7);
-		stack_clear(&a);
-		stack_clear(&b);
-		return (0);
+		printf("value=%d index=%d\n", cur->val, cur->idx);
+		cur = cur->next;
 	}
-	write(1, "NOT SORTED\n", 11);
+	// if (is_sorted(&a))
+	// {
+	// 	write(1, "SORTED\n", 7);
+	// 	stack_clear(&a);
+	// 	stack_clear(&b);
+	// 	return (0);
+	// }
+	// write(1, "NOT SORTED\n", 11);
 	stack_clear(&a);
 	stack_clear(&b);
 	return (0);
