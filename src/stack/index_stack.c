edd43df020105be6d37aa49905bce81e6f64ab3b
eepylaurie <eepylaurie@gmail.com>
2026-01-19 16:55:49 +0100

    Add index_stack function to handle indexing of stack elements

---
+++ src/stack/index_stack.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   index_stack.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/19 16:35:46 by lmatthes          #+#    #+#             */
+/*   Updated: 2026/01/19 16:42:38 by lmatthes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	swap_int(int *a, int *b)
+{
+	int	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+static void	sort_int_array(int *arr, int n)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < n - 1)
+	{
+		j = 0;
+		while (j < n - 1 - i)
+		{
+			if (arr[j] > arr[j + 1])
+				swap_int(&arr[j], &arr[j + 1]);
+			j++;
+		}
+		i++;
+	}
+}
+
+static int	find_pos(const int *arr, int n, int v)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (arr[i] == v)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+void	index_stack(t_stack *a)
+{
+	int		*arr;
+	int		n;
+	int		pos;
+	t_node	*cur;
+
+	arr = stack_to_array(a, &n);
+	sort_int_array(arr, n);
+	cur = a->top;
+	while (cur)
+	{
+		pos = find_pos(arr, n, cur->val);
+		cur->idx = pos;
+		cur = cur->next;
+	}
+	free(arr);
+}
