4261df5941ef9718398d1377e7d0ff19b7c4583e
eepylaurie <eepylaurie@gmail.com>
2026-01-20 14:07:17 +0100

    Add stack_info.c to merge stack_is_sorted and stack_len

--- src/stack/stack_info.c
+++ src/stack/stack_info.c
@@ -1,16 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   stack_len.c                                        :+:      :+:    :+:   */
+/*   stack_info.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/19 19:05:45 by lmatthes          #+#    #+#             */
-/*   Updated: 2026/01/19 19:07:00 by lmatthes         ###   ########.fr       */
+/*   Created: 2026/01/20 13:50:59 by lmatthes          #+#    #+#             */
+/*   Updated: 2026/01/20 14:06:08 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	stack_is_sorted(t_stack *s)
+{
+	t_node	*cur;
+
+	if (!s || s->size < 2)
+		return (1);
+	cur = s->top;
+	while (cur->next)
+	{
+		if (cur->val > cur->next->val)
+			return (0);
+		cur = cur->next;
+	}
+	return (1);
+}
 
 int	stack_len(t_stack *s)
 {
