d48c3a825d33d9e6db8ca24ec5135b354d0cc136
eepylaurie <eepylaurie@gmail.com>
2026-01-25 21:06:57 +0100

    Refactor parse_one_arg function for clarity and consistency in argument parsing

--- src/parse/parse_args.c
+++ src/parse/parse_args.c
@@ -6,7 +6,7 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 20:22:11 by lmatthes          #+#    #+#             */
-/*   Updated: 2026/01/19 22:52:07 by lmatthes         ###   ########.fr       */
+/*   Updated: 2026/01/25 21:06:00 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,54 +17,54 @@ static int	ft_isspace(int c)
 	return (c == ' ' || (c >= 9 && c <= 13));
 }
 
-static int	scan_one_arg(const char *s, t_stack *a)
+static int	parse_one_arg_into_stack(const char *s, t_stack *a)
 {
-	size_t	j;
-	size_t	start;
+	size_t	i;
+	size_t	token_start;
 	int		ok;
-	int		val;
-	int		found;
+	int		value;
+	int		found_number;
 
-	j = 0;
-	found = 0;
-	while (s[j])
+	i = 0;
+	found_number = 0;
+	while (s[i])
 	{
-		while (s[j] && ft_isspace(s[j]))
-			j++;
-		if (!s[j])
+		while (s[i] && ft_isspace(s[i]))
+			i++;
+		if (!s[i])
 			break ;
-		start = j;
-		while (s[j] && !ft_isspace(s[j]))
-			j++;
-		val = parse_atoi_strict_span(s + start, j - start, &ok);
+		token_start = i;
+		while (s[i] && !ft_isspace(s[i]))
+			i++;
+		value = parse_atoi_strict_span(s + token_start, i - token_start, &ok);
 		if (!ok)
 			error_exit();
-		stack_add_bottom(a, stack_node_new(val));
-		found = 1;
+		stack_add_bottom(a, stack_node_new(value));
+		found_number = 1;
 	}
-	return (found);
+	return (found_number);
 }
 
 void	parse_args(int argc, char **argv, t_stack *a)
 {
 	int	i;
-	int	found_any;
-	int	n;
-	int	*arr;
+	int	found_any_number;
+	int	count;
+	int	*values;
 
-	if (argc == 1)
+	if (argc <= 1)
 		return ;
+	found_any_number = 0;
 	i = 1;
-	found_any = 0;
 	while (i < argc)
 	{
-		if (scan_one_arg(argv[i], a))
-			found_any = 1;
+		if (parse_one_arg_into_stack(argv[i], a))
+			found_any_number = 1;
 		i++;
 	}
-	if (!found_any)
+	if (!found_any_number)
 		error_exit();
-	arr = stack_to_array(a, &n);
-	parse_check_duplicates(arr, n);
-	free(arr);
+	values = stack_to_array(a, &count);
+	parse_check_duplicates(values, count);
+	free(values);
 }
